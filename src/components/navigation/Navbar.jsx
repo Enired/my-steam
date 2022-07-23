d62a3b1af932eee28efc93b92d34813e540fef0b
Derine Banis <d.banis.13@gmail.com>
2022-07-23 03:16:46 -0700

    More navbar styling

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -8,8 +8,9 @@ const Navbar = (props) => {
 
   return(
     <nav className="navbar">
+      <h1 className="navTitle">My Games</h1>
       <ul>
-        <li onClick={()=>console.log('playing')}>Playing</li>
+        <li onClick={()=>console.log('playing')}>Playing </li>
         <li onClick={()=>console.log('on hold')}>On Hold</li>
         <li onClick={()=>console.log('completed')}>Completed</li>
         <li onClick={()=>console.log('dropped')}>Dropped</li>
