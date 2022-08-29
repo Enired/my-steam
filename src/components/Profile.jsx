8ad36f02dfd6d3893c0d978ecd98f3dc656e4647
Derine Banis <d.banis.13@gmail.com>
2022-08-29 15:43:45 -0700

    Add basic layout and elements for profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,19 +7,42 @@ export const Profile = () => {
   // Will use a search bar to dynamically change what user is loaded but for the time being. It will be my own. Enired.
 
   const [player, setPlayer] = useState({})
+  const [playerMeta, setPlayerMeta] = useState({})
 
   useEffect(()=>{
     const playerDBUrl = 'https://playerdb.co/api/player/steam/enired' //Change this later.
     axios.get(playerDBUrl)
     .then((res)=>{
       const player = res.data.data.player
+      console.log(player.meta) // Kept for testing purposes. REMOVE AFTER
       setPlayer(player)
+      setPlayerMeta(player.meta)
       })
   },[])
   
+
+  const username = player.username
+  const id = player.id
+  const avatar = player.avatar
+  const playerName = playerMeta.realname
+  const officialSteamProfile = playerMeta.profileurl
   return(
     <div>
-      {player.username}
+      <div>NAV BAR HERE AND WILL BE REPLACED</div>
+      <div className="profile-header">
+        <div className="persona-name">
+          {username}
+        </div>
+        <div className="real-name">
+          {playerName} 
+        </div>
+      </div>
+      <div className="profile-avatar">
+        <img src={avatar} alt="steam-profile-picture" />
+      </div>
+      <div className="offical-steam-profile-link">
+        {officialSteamProfile}
+      </div>
     </div>
   )
 
