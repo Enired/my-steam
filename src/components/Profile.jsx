35f12c9b66289ee0b067fce7ef452b272e3798ac
Derine Banis <d.banis.13@gmail.com>
2022-08-29 15:16:07 -0700

    Add useState for player state

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,25 @@
 import axios from "axios"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Profile = () => {
 
   // Fetching account info from player db
   // Will use a search bar to dynamically change what user is loaded but for the time being. It will be my own. Enired.
 
+  const [player, setPlayer] = useState({})
+
   useEffect(()=>{
     const playerDBUrl = 'https://playerdb.co/api/player/steam/enired' //Change this later.
     axios.get(playerDBUrl)
-    .then((data)=>{console.log(data)})
+    .then((res)=>{
+      const player = res.data.data.player
+      setPlayer(player)
+      })
   },[])
   
   return(
     <div>
-      
+      {player.username}
     </div>
   )
 
