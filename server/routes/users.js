d6d69ac9c8a5ce86bde4394d913ce68e44b5f6be
Derine Banis <d.banis.13@gmail.com>
2022-08-31 11:08:23 -0700

    Setup connection to ElephantSQL DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg')
+
+const dbConnectionString = process.env.ELEPHANT_DB_URL
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const client = new pg.Client(dbConnectionString)
+  client.connect((err)=>{
+    if(err){
+      return console.error('couldn\'t connect to postgres', err)
+    }
+    client.query('DROP TABLE IF EXISTS "users"; CREATE TABLE IF NOT EXISTS"users"(user_id serial PRIMARY KEY, name VARCHAR(50) )', (err, result)=>{
+      if(err){
+        return console.error('error running query', err)
+      }
+      console.log('donesies');
+      client.end();
+    })
+  })
+
+  res.send("Done")
 });
 
 module.exports = router;
