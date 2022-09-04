6e8d72db41ef30ba381479b9292053341d98a223
Derine Banis <d.banis.13@gmail.com>
2022-09-04 11:05:33 -0700

    Add nav-menu to nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import "../styles/Navbar.scss"
 import mslLogoOnly from "../assets/mysteam-logo-only.png"
 import mslLogo from "../assets/mysteamlistlogowhite.png"
 import mslLogoNav from "../assets/mysteam-logo-nav.png"
+import MenuIcon from '@mui/icons-material/Menu';
 export const Navbar = (props) => {
 
   return(
     <div id="navbar">
+      <MenuIcon id="nav-menu"/>
       <img src={mslLogoNav} alt="logo" id="nav-logo"className="logo" />
     </div>
   )
