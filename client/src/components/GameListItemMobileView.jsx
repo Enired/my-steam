4b11b16dc3a0111db16e089502e0859f8ded8ad3
Derine Banis <d.banis.13@gmail.com>
2022-09-05 15:15:01 -0700

    Add styling to table

--- client/src/components/GameListItemMobileView.jsx
+++ client/src/components/GameListItemMobileView.jsx
@@ -4,16 +4,15 @@ export const GameListItemMobileView = (props) => {
       <td>
         <table>
           <tbody>
-            <tr>
-              <th className="game-list-table-header">Game Icon:</th>
-              <td className="game-list-table-data-item"><img src={props.gamePic} alt="Game Icon" /></td>
+            <tr className="game-list-mobile">
+              <td className="game-list-table-data-item game-list-table-img"><img src={props.gamePic} alt="Game Icon" /></td>
             </tr>
             <tr>
-              <th className="game-list-table-header">Game Name:</th>
+              <th className="game-list-table-header">Name:</th>
               <td className="game-list-table-data-item">{props.gameName}</td>
             </tr>
             <tr>
-              <th className="game-list-table-header">Game Status:</th>
+              <th className="game-list-table-header">Status:</th>
               <td className="game-list-table-data-item">{props.gameStatus}</td>
             </tr>
           </tbody>
