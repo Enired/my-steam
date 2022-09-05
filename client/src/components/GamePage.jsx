21a0ae32e5a31169c06ba277622c55cf0a34f2e3
Derine Banis <d.banis.13@gmail.com>
2022-09-05 13:45:31 -0700

    Refactor table

--- client/src/components/GamePage.jsx
+++ client/src/components/GamePage.jsx
@@ -4,32 +4,52 @@ export const GamePage = (props) => {
 
   const appid = 3830
   const hash = `460b6471db7d83ee6943c1a87f7a9f2898634952`
-  const gameName = 'Psychonauts'
   const gameStatus = 'Plan to play'
 
+
+  const gamesList = [
+    {
+      gameName : 'Psychonauts',
+      appid: 3830,
+      hash: '460b6471db7d83ee6943c1a87f7a9f2898634952',
+      gameStatus:'Plan to play'
+
+    },
+    {
+      gameName : 'Dead Space 2',
+      appid: 47780,
+      hash: '6393351676edc4fdc65937a599780818fd2f18b7',
+      gameStatus:'Plan to play'
+
+    },
+    {
+      gameName : 'SpaceChem',
+      appid: 92800,
+      hash: '29b26fc0eb22ebbeeeed1e4658dcbc6b837279c7',
+      gameStatus:'Plan to play'
+
+    }
+
+  ]
+
   return (
-    <div class="game-page">
+    <div className="game-page">
       <table className="game-list">
+        <tbody>
         <tr className="game-list-table-headers">
           <th className="game-list-table-header">Game Pic</th>
           <th className="game-list-table-header">Game Name</th>
           <th className="game-list-table-header">Game Status</th>
         </tr>
-        <tr className="game-list-table-data">
-          <td className="game-list-table-data-item game-list-table-img"><img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${appid}/${hash}.jpg`} alt="" /></td>
-          <td className="game-list-table-data-item">{gameName}</td>
-          <td className="game-list-table-data-item">{gameStatus}</td>
-        </tr>
-        <tr className="game-list-table-data">
-          <td className="game-list-table-data-item game-list-table-img"><img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${appid}/${hash}.jpg`} alt="" /></td>
-          <td className="game-list-table-data-item">{gameName}</td>
-          <td className="game-list-table-data-item">{gameStatus}</td>
-        </tr>
-        <tr className="game-list-table-data">
-          <td className="game-list-table-data-item game-list-table-img"><img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${appid}/${hash}.jpg`} alt="" /></td>
-          <td className="game-list-table-data-item">{gameName}</td>
-          <td className="game-list-table-data-item">{gameStatus}</td>
-        </tr>
+        {gamesList.map(game => {
+          return(
+        <tr className="game-list-table-data" key={game.appid}>
+          <td className="game-list-table-data-item game-list-table-img"><img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.hash}.jpg`} alt="" /></td>
+          <td className="game-list-table-data-item">{game.gameName}</td>
+          <td className="game-list-table-data-item">{game.gameStatus}</td>
+        </tr>)
+        })}
+        </tbody>
       </table>
     </div>
   )
