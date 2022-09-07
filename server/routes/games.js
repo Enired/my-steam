6ca3d6970d27ddf0d578bba155be36961bb01abb
Derine Banis <d.banis.13@gmail.com>
2022-09-07 11:57:04 -0700

    Complete game update feature

--- server/routes/games.js
+++ server/routes/games.js
@@ -111,43 +111,93 @@ router.get('/all', (req,res) => {
 
   })
 })
+
+
 router.put('/:id/current', (req,res) => {
   const client = new pg.Client(dbConnectionString);
   console.log(req.params.id)
-  // client.connect((err)=>{
-  //   if (err) {
-  //     return console.error('couldn\'t connect to postgres', err);
-  //   }
+  client.connect((err)=>{
+    if (err) {
+      return console.error('couldn\'t connect to postgres', err);
+    }
 
-  //   client.query(
-  //     `UPDATE games SET status = 'completed' where id= '$1'`,[req.id], (err, result) => {
-  //       if (err) {
-  //         return console.error('error running query', err);
-  //       }
-  //       console.log(result)
-  //       res.json(result.rows)
-  //       client.end()
+    client.query(
+      `UPDATE games SET status = 'current' where id= $1`,[req.params.id], (err, result) => {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        console.log(result)
+        client.end()
+        res.status(200)
+      }
+    );
 
-  //     }
-  //   );
-
-  // })
-  res.send(`Set to Current`)
+  })
 })
 router.put('/:id/completed', (req,res) => {
   const client = new pg.Client(dbConnectionString);
   console.log(req.params.id)
-  res.send(`Set to Completed`)
+  client.connect((err)=>{
+    if (err) {
+      return console.error('couldn\'t connect to postgres', err);
+    }
+
+    client.query(
+      `UPDATE games SET status = 'completed' where id= $1`,[req.params.id], (err, result) => {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        console.log(result)
+        client.end()
+        res.status(200)
+      }
+    );
+
+  })
 })
+
 router.put('/:id/dropped', (req,res) => {
   const client = new pg.Client(dbConnectionString);
   console.log(req.params.id)
-  res.send(`Set to Dropped`)
+  client.connect((err)=>{
+    if (err) {
+      return console.error('couldn\'t connect to postgres', err);
+    }
+
+    client.query(
+      `UPDATE games SET status = 'dropped' where id= $1`,[req.params.id], (err, result) => {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        console.log(result)
+        client.end()
+        res.status(200)
+      }
+    );
+
+  })
 })
+
 router.put('/:id/planned', (req,res) => {
   const client = new pg.Client(dbConnectionString);
   console.log(req.params.id)
-  res.send(`Set to Planned`)
+  client.connect((err)=>{
+    if (err) {
+      return console.error('couldn\'t connect to postgres', err);
+    }
+
+    client.query(
+      `UPDATE games SET status = 'planned' where id= $1`,[req.params.id], (err, result) => {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        console.log(result)
+        client.end()
+        res.status(200)
+      }
+    );
+
+  })
 })
 
 module.exports = router;
