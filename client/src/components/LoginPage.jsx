475ec1cdf1e81a07e4fbb053a770f7a7d6a65626
Derine Banis <d.banis.13@gmail.com>
2022-09-19 13:36:53 -0700

    Add buttons to login page. Fix styling

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -3,10 +3,17 @@ import "../styles/LoginPage.scss"
 export const LoginPage = (props) => {
   return (
     <div className="login">
-      <label htmlFor="login-name">Username</label>
-      <input type="text" name="login-name" id="login-name" />
-      <label htmlFor="login-password">Password</label>
-      <input type="password" name="login-password" id="login-password"/>    
+      <div className="login-text">
+        <label htmlFor="login-name">Username</label>
+        <input type="text" name="login-name" id="login-name" />
+        <label htmlFor="login-password">Password</label>
+        <input type="password" name="login-password" id="login-password"/>    
+      </div>
+      <div className="login-buttons">
+        <button>Login</button>
+        <button>Sign-up</button>
+        <button>Forgot Password</button>
+      </div>
     </div>
   
   )
