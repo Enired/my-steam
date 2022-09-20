bcf35c19d1a68a0ed73d9390ec2337151cd188c5
Derine Banis <d.banis.13@gmail.com>
2022-09-20 15:36:10 -0700

    Add relevant fields to signup page

--- client/src/components/SignupPage.jsx
+++ client/src/components/SignupPage.jsx
@@ -3,9 +3,17 @@ import "../styles/SignupPage.scss"
 export const SignupPage = (props) => {
   return (
     <div className="signup-page">
+      <h2>Sign-up</h2>
       <div className="signup-fields">
+        <label htmlFor="username-creation">Username</label>
         <input type="text" className="signup-field" id="username-creation" />
-        <input type="text" className="signup-field" id="password-creation" />
+        <label htmlFor="password-creation">Password</label>
+        <input type="password" className="signup-field" id="password-creation" />
+        <label htmlFor="password-confirmation">Confirm Password</label>
+        <input type="password" className="signup-field" id="password-confirmation" />
+        <label htmlFor="email-creation">Steam ID</label>
+        <input type="text" className="signup-field" id="steam-id-import" placeholder="76561198008227465"/>
+
       </div>
       <div className="signup-buttons">
         <button className="signup-button">Create Account</button>
