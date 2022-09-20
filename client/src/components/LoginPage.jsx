3c525796a94e2e5cd665ec2c4488d6d43616c989
Derine Banis <d.banis.13@gmail.com>
2022-09-20 13:22:01 -0700

    Prevent login if username and password field on login is blank

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import "../styles/LoginPage.scss"
 
 export const LoginPage = (props) => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkIfBlank = (textInput) => {
+    if(!textInput){
+      return true
+    }
+
+    return false
+  }
+
+
   return (
     <div className="login">
       <div className="login-text">
         <label htmlFor="login-name">Username</label>
-        <input type="text" name="login-name" id="login-name" />
+        <input type="text" name="login-name" id="login-name" value={username} onChange={(event)=>{setUsername(event.target.value)}}/>
         <label htmlFor="login-password">Password</label>
-        <input type="password" name="login-password" id="login-password"/>    
+        <input type="password" name="login-password" id="login-password" value={password} onChange={(event)=>{setPassword(event.target.value)}}/>    
       </div>
       <div className="login-buttons">
-        <button className="action-button" id="login-button" onClick={()=>{props.switchViewLogin(); props.setLoggedIn(true)}}>Login</button>
+        <button className="action-button" id="login-button" onClick={()=>{(!checkIfBlank(username) & !checkIfBlank(password)) && props.switchViewLogin(); props.setLoggedIn(true)}}>Login</button>
         <button className="action-button" id="sign-up-button">Sign-up</button>
         <button className="action-button" id="forgot-password-button">Forgot Password</button>
       </div>
