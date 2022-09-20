dbac8d5f458b4ad4b54cebc88166b0f3312cfc53
Derine Banis <d.banis.13@gmail.com>
2022-09-20 15:38:17 -0700

    Add placeholders to signup text fields

--- client/src/components/SignupPage.jsx
+++ client/src/components/SignupPage.jsx
@@ -6,11 +6,11 @@ export const SignupPage = (props) => {
       <h2>Sign-up</h2>
       <div className="signup-fields">
         <label htmlFor="username-creation">Username</label>
-        <input type="text" className="signup-field" id="username-creation" />
+        <input type="text" className="signup-field" id="username-creation" placeholder="Username"/>
         <label htmlFor="password-creation">Password</label>
-        <input type="password" className="signup-field" id="password-creation" />
+        <input type="password" className="signup-field" id="password-creation" placeholder="Password" />
         <label htmlFor="password-confirmation">Confirm Password</label>
-        <input type="password" className="signup-field" id="password-confirmation" />
+        <input type="password" className="signup-field" id="password-confirmation" placeholder="Confirm Password"/>
         <label htmlFor="email-creation">Steam ID</label>
         <input type="text" className="signup-field" id="steam-id-import" placeholder="76561198008227465"/>
 
