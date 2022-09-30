371455b9240930ff3077c15a3a9f55e157dd6f78
Derine Banis <d.banis.13@gmail.com>
2022-09-30 10:13:25 -0700

    Set up for hosting

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { Navbar } from './components/Navbar';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 root.render(
   <React.StrictMode>
     <App />
