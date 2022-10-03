6b03981d6c7dfd282447f32818725349ef9455c3
Derine Banis <d.banis.13@gmail.com>
2022-10-03 14:31:34 -0700

    Implement pagination

--- client/src/components/GamePage.jsx
+++ client/src/components/GamePage.jsx
@@ -2,26 +2,39 @@ import "../styles/GamePage.scss";
 
 import { GameListItemMobileView } from "./GameListItemMobileView";
 import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp';
+import ArrowBackIosNewSharpIcon from '@mui/icons-material/ArrowBackIosNewSharp';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const GamePage = (props) => {
 
   const gamesList = props.gamesList
+  const [currentPage, setCurrentPage] = useState(1)
+  const firstItem = (currentPage-1)*10
+  const lastItem = currentPage*10
 
   //Capitalize the first letter of the game status.
   const capStatus = (str) => {
     return `${str.substring(0,1).toUpperCase()}${str.substring(1).toLowerCase()}`
   } 
 
+  const back = () => {
+    setCurrentPage(currentPage - 1);
+    window.scrollTo(0,0)
+  }
+  
+  const forward = () => {
+    setCurrentPage(currentPage + 1);
+    window.scrollTo(0,0)
+  }
+
+  useEffect(()=>{},[currentPage])
   return (
     <div className="game-page">
       <table className="game-list">
         <tbody>
-          {/* <tr className="game-list-table-headers">
-          <th className="game-list-table-header">Game Pic</th>
-          <th className="game-list-table-header">Game Name</th>
-          <th className="game-list-table-header">Game Status</th>
-        </tr> */}
-          {gamesList.map(game => {
+          {gamesList.slice(firstItem, lastItem).map(game => {
     
               return (<GameListItemMobileView
                 key={game.game_list_item_id}
@@ -30,17 +43,16 @@ export const GamePage = (props) => {
                 gameName={game.game_name}
                 gameStatus={capStatus(game.game_status)}
               />
-                // <tr className="game-list-table-data" key={game.appid}>
-                //   <td className="game-list-table-data-item game-list-table-img"><img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.hash}.jpg`} alt="" /></td>
-                //   <td className="game-list-table-data-item">{game.gameName}</td>
-                //   <td className="game-list-table-data-item">{game.gameStatus}</td>
-                // </tr>
               );
             
           })}
         </tbody>
       </table>
-      <ArrowBackSharpIcon id="back-to-profile-button" onClick={()=>{props.switchView()}}></ArrowBackSharpIcon>
+      <div className="pagination-buttons">
+      {currentPage > 1 && <ArrowBackIosNewSharpIcon className="pagination-navigation" onClick={()=>back()}>BACK</ArrowBackIosNewSharpIcon>}
+      {(gamesList.length > 10 && lastItem < gamesList.length) && <ArrowForwardIosSharpIcon className="pagination-navigation" onClick={()=>forward()}/>}
+      </div>
+      <ArrowBackSharpIcon id="back-to-profile-button"  onClick={()=>{props.switchView()}}></ArrowBackSharpIcon>
     </div>
   );
 };
